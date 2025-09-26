59121ef8d1fb74aed090c09fe41b1e690a1c1f8a
Markus Jylhänkangas <markus@jylhis.com>
2025-09-26 19:47:30 +0200

    Initial repository with flake-parts

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Marchyo";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    # flake-compat.url = "github:nix-community/flake-compat";
+    # flake-utils.url = "github:numtide/flake-utils";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      imports = [
+        inputs.flake-parts.flakeModules.flakeModules
+        inputs.disko.flakeModules.default
+        inputs.home-manager.flakeModules.home-manager
+        inputs.treefmt-nix.flakeModule
+
+        ./modules/flake
+      ];
+
+    };
+}
