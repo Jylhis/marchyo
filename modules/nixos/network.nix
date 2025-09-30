e43e3773cb46d13acf65014f473b9642d42b0e49
Markus Jylhänkangas <markus@jylhis.com>
2025-09-30 19:12:22 +0200

    iwd backend

--- modules/nixos/network.nix
+++ modules/nixos/network.nix
@@ -3,11 +3,10 @@
   # services.resolved.enable = true;
   networking = {
     networkmanager.enable = true;
+    wifi.backend = "iwd";
   };
   environment.systemPackages = with pkgs; [
     impala # TUI for managing your Wi-Fi connection, NOTE: doesnt support network manager
     # lazyssh # not available in 25.05
   ];
-
-  # TODO: WIFI
 }
