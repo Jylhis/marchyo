6eb4f5f9adea21460b375fe5cf364abcf7518b2e
Markus Jylhänkangas <markus@jylhis.com>
2025-10-04 14:02:12 +0200

    fixup! Cleanup

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -10,7 +10,5 @@
   environment.systemPackages = with pkgs; [
     sysz # systemctl tui
     lazyjournal # journald and logs
-    fastfetch # shows system information
-    btop # beautiful resource manager
   ];
 }
