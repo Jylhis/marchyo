caab57a841ad5b4c05faadafa7c84b4d5f679bc0
Markus Jylhänkangas <markus@jylhis.com>
2025-10-05 11:19:39 +0200

    Remove iwd

--- modules/nixos/network.nix
+++ modules/nixos/network.nix
@@ -1,18 +1,11 @@
 { pkgs, ... }:
 {
   networking = {
-    wireless = {
-      iwd = {
-        enable = true;
-      };
-    };
     networkmanager = {
       enable = true;
-      wifi.backend = "iwd";
     };
   };
   environment.systemPackages = with pkgs; [
-    impala # TUI for managing your Wi-Fi connection, NOTE: doesnt support network manager
     # lazyssh # not available in 25.05
   ];
 }
