2dc4fe4073235a75c36e87ce2e240a7201327698
Markus Jylhänkangas <markus@jylhis.com>
2025-10-23 08:59:52 +0200

    fiz fzf

--- modules/home/fzf.nix
+++ modules/home/fzf.nix
@@ -16,18 +16,18 @@ in
       colors = mkIf (cfg != null && cfg.enable && colors != null) (
         with colors;
         {
-          "bg" = base00;
-          "bg+" = base01;
-          "fg" = base04;
-          "fg+" = base06;
-          "header" = base0D;
-          "hl" = base0D;
-          "hl+" = base0D;
-          "info" = base0A;
-          "marker" = base0C;
-          "pointer" = base0C;
-          "prompt" = base0A;
-          "spinner" = base0C;
+          "bg" = "#${base00}";
+          "bg+" = "#${base01}";
+          "fg" = "#${base04}";
+          "fg+" = "#${base06}";
+          "header" = "#${base0D}";
+          "hl" = "#${base0D}";
+          "hl+" = "#${base0D}";
+          "info" = "#${base0A}";
+          "marker" = "#${base0C}";
+          "pointer" = "#${base0C}";
+          "prompt" = "#${base0A}";
+          "spinner" = "#${base0C}";
         }
       );
     };
