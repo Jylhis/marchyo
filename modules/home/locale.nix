9ae21d5d0d36b868468c92f501b30a0b679d9ab0
Markus Jylhänkangas <markus@jylhis.com>
2025-11-15 21:02:43 +0100

    Remove commented out options

--- modules/home/locale.nix
+++ modules/home/locale.nix
@@ -1,17 +1,5 @@
 { lib, osConfig, ... }:
 {
-  i18n = {
-    # FIXME: ctrl+;
-    #inputMethod = {
-    #  enable = true;
-    #type = "fcitx5";
-    #fcitx5.addons = with pkgs; [
-    #  fcitx5-mozc
-    #  fcitx5-gtk
-    #];
-    # };
-  };
-
   # Home Manager language configuration - use osConfig to get marchyo settings
   home.language = lib.mkDefault {
     base = osConfig.marchyo.defaultLocale;
--- modules/nixos/locale.nix
+++ modules/nixos/locale.nix
@@ -4,14 +4,5 @@
   i18n = {
     defaultLocale = lib.mkDefault config.marchyo.defaultLocale;
     supportedLocales = [ "all" ];
-    # FIXME: ctrl+;
-    #inputMethod = {
-    #  enable = true;
-    #  type = "fcitx5";
-    #  fcitx5.addons = with pkgs; [
-    #    fcitx5-mozc
-    #    fcitx5-gtk
-    #  ];
-    #};
   };
 }
