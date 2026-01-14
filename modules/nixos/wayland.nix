4fcc3f67b5f24215d72d7d28772a8a191ab44f35
Markus Jylhänkangas <markus@jylhis.com>
2026-01-14 18:25:10 +0100

    Enable wayland support for applications

--- modules/nixos/wayland.nix
+++ modules/nixos/wayland.nix
@@ -1,10 +1,7 @@
 {
   environment.sessionVariables = {
-    # NIXOS_OZONE_WL = "1";
-    # MOZ_ENABLE_WAYLAND = "1";
-    # ELECTRON_OZONE_PLATFORM_HINT = "wayland";
-    # XDG_CURRENT_DESKTOP = "Hyprland";
-    # XDG_SESSION_DESKTOP = "Hyprland";
-    # XDG_SESSION_TYPE = "wayland";
+    # Conservative Wayland settings
+    MOZ_ENABLE_WAYLAND = "1";
+    ELECTRON_OZONE_PLATFORM_HINT = "auto";
   };
 }
