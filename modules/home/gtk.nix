f5045fcd691a320272197454856fee56ade9e40e
Markus Jylhänkangas <markus@jylhis.com>
2026-04-08 15:34:31 +0200

    fix(gtk): silence gtk4.theme deprecation warning for stateVersion < 26.05
    Set gtk.gtk4.theme to legacy default (config.gtk.theme) unconditionally
    with mkDefault so consumers can override, instead of only setting it
    when desktop is enabled.

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -10,21 +10,25 @@ let
   home = config.home.homeDirectory;
 in
 {
-  config = lib.mkIf desktopEnabled {
-    gtk = {
-      gtk4.theme = null;
-      iconTheme = {
-        package = pkgs.adwaita-icon-theme;
-        name = "Adwaita";
+  config = lib.mkMerge [
+    {
+      gtk.gtk4.theme = lib.mkDefault config.gtk.theme;
+    }
+    (lib.mkIf desktopEnabled {
+      gtk = {
+        iconTheme = {
+          package = pkgs.adwaita-icon-theme;
+          name = "Adwaita";
+        };
+        gtk3.bookmarks = [
+          "file://${config.xdg.userDirs.documents}"
+          "file://${config.xdg.userDirs.download}"
+          "file://${config.xdg.userDirs.music}"
+          "file://${config.xdg.userDirs.pictures}"
+          "file://${config.xdg.userDirs.videos}"
+          "file://${home}/Developer"
+        ];
       };
-      gtk3.bookmarks = [
-        "file://${config.xdg.userDirs.documents}"
-        "file://${config.xdg.userDirs.download}"
-        "file://${config.xdg.userDirs.music}"
-        "file://${config.xdg.userDirs.pictures}"
-        "file://${config.xdg.userDirs.videos}"
-        "file://${home}/Developer"
-      ];
-    };
-  };
+    })
+  ];
 }
